0402ddeeec6bc9e14e87a76d4761bdf7ad228471
fifinaaa <selange@stud.mediadesign.de>
2025-07-22 12:12:16 +0200

    ImGui Wrapper

--- Flux/src/Flux/ImGui/ImGuiWrapper.cpp
+++ Flux/src/Flux/ImGui/ImGuiWrapper.cpp
@@ -5,9 +5,20 @@
 namespace Flux::ImGuiWrapper 
 {
 
-	void Begin(const char* name) 
-	{ 
-		ImGui::Begin(name); 
+	void Begin(
+		const char* _name,
+		ImGuiWindowFlags _flags = 0,
+		ImVec2* _size = nullptr,
+		ImVec2* _pos = nullptr,
+		ImGuiCond _posCond = ImGuiCond_Once,
+		ImGuiCond _sizeCond = ImGuiCond_Once)
+	{
+		if (_pos)
+			ImGui::SetNextWindowPos(*_pos, _posCond);
+		if (_size)
+			ImGui::SetNextWindowSize(*_size, _sizeCond);
+
+		ImGui::Begin(_name, nullptr, _flags);
 	}
 
 	void Text(const char* fmt, ...) 
@@ -21,5 +32,10 @@ namespace Flux::ImGuiWrapper
 	void End() 
 	{ 
 		ImGui::End(); 
+	}
+
+	void Button(const char* _buttonName)
+	{
+		ImGui::Button(_buttonName);
 	}
 }
--- Flux/src/Flux/ImGui/ImGuiWrapper.h
+++ Flux/src/Flux/ImGui/ImGuiWrapper.h
@@ -7,6 +7,7 @@ namespace Flux::ImGuiWrapper
 	FLUX_API void Begin(const char* name);
 	FLUX_API void Text(const char* fmt, ...);
 	FLUX_API void End();
+	FLUX_API void Button(const char* _buttonName);
 }
 
 
