4620360677f67be040596f8399c8b4fb4b7e7e09
oelwechsel <129411835+oelwechsel@users.noreply.github.com>
2025-08-08 00:15:28 +0200

    added InGame File System for NPCS

--- @venture/src/Scripts/GameManagerScript.h
+++ @venture/src/Scripts/GameManagerScript.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <Flux.h>
 
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+
+namespace fs = std::filesystem;
+
 class GameManagerScript : public Flux::IScript
 {
 public:
@@ -26,6 +32,75 @@ public:
         return *s_Instance;
     }
 
+
+    void CreateGameFolderStructure() {
+        fs::path exePath = fs::current_path();
+        fs::path homePath = exePath / "home";
+        fs::create_directories(homePath);
+
+        // Ordner + Textdateien mit Inhalt
+        std::vector<std::tuple<std::string, std::string, std::string>> folders = {
+            { "@",  "welcome_message.txt",  "What are you doing here?! Get back to the Game!\n" },
+            { "equals",    "welcome_message.txt",    "Oh man, now we've really done it \n *sweating profusely*\n" },
+            { "exclamation_mark", "welcome_message.txt", "*sob* *sob* *sob*\n" },
+            { "question_mark", "welcome_message.txt", "Wanna learn something good, kid ? \n At some point you will need SUDO\n" },
+            { "and", "welcome_message.txt", "Everyone is gone... I will be gone too\n" },
+            { "percent", "welcome_message.txt", "Pssssssssssst look at what I got\n" } 
+        };
+
+        for (const auto& [folderName, fileName, fileContent] : folders) {
+            fs::path folderPath = homePath / folderName;
+            fs::create_directories(folderPath);
+
+            fs::path filePath = folderPath / fileName;
+
+            std::ofstream out(filePath);
+            if (out.is_open()) {
+                out << fileContent;
+                out.close();
+            }
+        }
+
+        fs::path current = homePath / "percent" / "secret";
+        std::vector<std::string> layers = {
+            "superSecret",
+            "superSuperSecret",
+            "reallyASecret",
+            "PleaseDontTellRootTypaSecret",
+            "KeepOut!"
+        };
+
+        for (const auto& layer : layers) {
+            current /= layer;
+            fs::create_directories(current);
+        }
+
+        fs::path secretFile = current / "password.txt";
+        std::ofstream secretOut(secretFile);
+        if (secretOut.is_open()) {
+            secretOut << "cGFzc3dvcnQ=\n";  
+            secretOut.close();
+        }
+    }
+
+    std::vector<std::string> LoadDialogueLinesForSymbol(const std::string& folderName)
+    {
+        fs::path homePath = fs::current_path() / "home";
+        fs::path filePath = homePath / folderName / "welcome_message.txt";
+
+        bool success = false;
+        auto lines = Flux::FileLoader::LoadTextFile(filePath.string(), &success);
+
+        if (!success)
+        {
+            FX_CORE_WARN("Could not load dialogue lines for folder '{}'", folderName);
+            return {};
+        }
+
+        return lines;
+    }
+
+
     void Start() override
     {
         if (s_Instance && s_Instance != this)
@@ -37,33 +112,35 @@ public:
 
         s_Instance = this;
 
+        CreateGameFolderStructure();
+
         // Rest des Codes
         Flux::Entity player(14, 14, '@', "@", true);
         entities.push_back(player);
 
         Flux::Entity questionNPC(33, 32, '?', "?", 0.2f, ImVec2(180, 370), false);
         questionNPC.layeredFrames = Flux::FileLoader::LoadAsciiFrames("QUEST_Animation.txt");
-        questionNPC.dialogueLines = { "Wanna learn something good, kid?", "At some point you will need SUDO" };
+        questionNPC.dialogueLines = LoadDialogueLinesForSymbol("question_mark");
         entities.push_back(questionNPC);
 
         Flux::Entity andNPC(11, 9, '&', "&", 0.2f, ImVec2(240, 350), false);
         andNPC.layeredFrames = Flux::FileLoader::LoadAsciiFrames("AND_Animation.txt");
-        andNPC.dialogueLines = { "Everyone is gone... I will be gone too" };
+        andNPC.dialogueLines = LoadDialogueLinesForSymbol("and");
         entities.push_back(andNPC);
 
         Flux::Entity percentNPC(7, 23, '%', "%", 0.2f, ImVec2(300, 450), false);
         percentNPC.layeredFrames = Flux::FileLoader::LoadAsciiFrames("PERC_Animation.txt");
-        percentNPC.dialogueLines = { "Pssssssssssst look at what I got" };
+        percentNPC.dialogueLines = LoadDialogueLinesForSymbol("percent");
         entities.push_back(percentNPC);
 
         Flux::Entity equalsNPC(20, 20, 'a', "=", 0.2f, ImVec2(230, 350), false);
         equalsNPC.layeredFrames = Flux::FileLoader::LoadAsciiFrames("EQUA_Animation.txt");
-        equalsNPC.dialogueLines = { "Oh man, now we've really done it", "*sweating profusely*" };
+        equalsNPC.dialogueLines = LoadDialogueLinesForSymbol("equals");
         entities.push_back(equalsNPC);
 
         Flux::Entity exclamationNPC(18, 14, '!', "!", 0.3f, ImVec2(170, 390), false);
         exclamationNPC.layeredFrames = Flux::FileLoader::LoadAsciiFrames("EXCL_Animation.txt");
-        exclamationNPC.dialogueLines = { "*sob* *sob* *sob*" };
+        exclamationNPC.dialogueLines = LoadDialogueLinesForSymbol("exclamation_mark");
         entities.push_back(exclamationNPC);
     }
 
